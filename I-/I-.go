172b8cee6c6448494dfc5810de779177da052873
Yuri Gorshenin <y@mmaps.me>
2015-06-19 23:57:47 +0300

    Added I- script.

---
+++ I-/I-.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	acme "github.com/ygorshenin/acmescripts"
+)
+
+func usage() {
+	fmt.Fprintln(os.Stderr, "Usage: %s [num-spaces]", os.Args[0])
+	os.Exit(-1)
+}
+
+func removePrefix(data []byte, numSpaces int) []byte {
+	if len(data) == 0 {
+		return data
+	}
+
+	var prefix []byte = []byte(strings.Repeat(" ", numSpaces))
+	var sep []byte = []byte("\n")
+
+	var result = make([]byte, 0)
+	for _, line := range bytes.Split(data, sep) {
+		result = append(result, bytes.TrimPrefix(line, prefix)...)
+		result = append(result, '\n')
+	}
+	return result[0 : len(result)-1]
+}
+
+func main() {
+	var numSpaces int = 2
+
+	switch len(os.Args) {
+	case 1:
+	case 2:
+		numSpaces64, err := strconv.ParseInt(os.Args[1], 10 /* base */, 0 /* bits */)
+		numSpaces = int(numSpaces64)
+		if err == nil && numSpaces < 0 {
+			err = errors.New("Negative numSpaces are disallowed.")
+		}
+		acme.CheckError(err, "Can't parse command line argument:", os.Args[1])
+	default:
+		usage()
+	}
+
+	win := acme.GetCurrentWindow()
+	defer win.CloseFiles()
+
+	dot := acme.ReadDot(win)
+	start, _ := acme.ReadAddr(win)
+
+	// Removes spaces from dot.
+	dot = removePrefix(dot, numSpaces)
+	acme.Write(win, "data", dot)
+	acme.WriteAddr(win, "#%v,#%v", start, start+len(dot))
+	acme.Ctl(win, "dot=addr")
+}
